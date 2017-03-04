3c483ac4ef75e79f67e67bcf580981ce41067f48
Michael Robinson <seraph890@gmail.com>
2017-03-04 21:11:24 +0900

    Updated game screen
    GameScreen now has implemented scrolling for large worlds, it is also
    able to take input for movement.
    Creates new world with cave implementation.

--- src/main/groovy/com/michaelr/screens/GameScreen.groovy
+++ src/main/groovy/com/michaelr/screens/GameScreen.groovy
@@ -2,17 +2,76 @@ package com.michaelr.screens
 
 import java.awt.event.KeyEvent
 import asciiPanel.AsciiPanel
+import com.michaelr.World
+import com.michaelr.WorldBuilder
 
 class GameScreen implements Screen {
+  private World world
+  private int centerX
+  private int centerY
+  private int screenWidth
+  private int screenHeight
+
+  GameScreen(){
+      screenWidth = 80
+      screenHeight = 21
+      createWorld()
+  }
+
+  private void createWorld(){
+    world = new WorldBuilder(90, 31).makeCaves().build()
+  }
+
+  int getScrollX() {
+    Math.max(0, Math.min(centerX - screenWidth / 2, world.width() - screenWidth))
+  }
+
+  int getScrollY() {
+    Math.max(0, Math.min(centerY - screenHeight / 2, world.height() - screenHeight))
+  }
+
   void displayOutput(AsciiPanel terminal) {
-    terminal.write("You are having fun.", 1, 1)
+    int left = getScrollX()
+    int top = getScrollY()
+
+    displayTiles(terminal, left, top)
+
+    terminal.write('X', centerX - left, centerY - top)
     terminal.writeCenter("-- press [escape] to lose or [enter] to win --", 22)
+  }
+
+  private void displayTiles(AsciiPanel terminal, int left, int top) {
+    for (int x = 0; x < screenWidth; x++) {
+      for (int y = 0; y < screenHeight; y++) {
+        int wx = x + left
+        int wy = y + top
+
+        terminal.write(world.glyph(wx, wy), x, y, world.color(wx, wy))
+      }
+    }
+  }
+
+  private void scrollBy(int mx, int my){
+    centerX = Math.max(0, Math.min(centerX + mx, world.width() - 1))
+    centerY = Math.max(0, Math.min(centerY + my, world.height() - 1))
   }
 
   Screen respondToUserInput(KeyEvent key) {
     switch (key.getKeyCode()) {
       case KeyEvent.VK_ESCAPE: return new LoseScreen()
       case KeyEvent.VK_ENTER: return new WinScreen()
+      case KeyEvent.VK_LEFT:
+      case KeyEvent.VK_H: scrollBy(-1, 0); break
+      case KeyEvent.VK_RIGHT:
+      case KeyEvent.VK_L: scrollBy( 1, 0); break
+      case KeyEvent.VK_UP:
+      case KeyEvent.VK_K: scrollBy( 0,-1); break
+      case KeyEvent.VK_DOWN:
+      case KeyEvent.VK_J: scrollBy( 0, 1); break
+      case KeyEvent.VK_Y: scrollBy(-1,-1); break
+      case KeyEvent.VK_U: scrollBy( 1,-1); break
+      case KeyEvent.VK_B: scrollBy(-1, 1); break
+      case KeyEvent.VK_N: scrollBy( 1, 1); break
     }
 
     this
