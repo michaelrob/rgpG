6b2de09f2dd8efdabf86f84bc67951f8be1ecce1
Michael Robinson <seraph890@gmail.com>
2017-03-04 21:10:14 +0900

    Added basic code to implement for worlds

--- src/main/groovy/com/michaelr/World.groovy
+++ src/main/groovy/com/michaelr/World.groovy
@@ -12,3 +12,22 @@ class World {
     this.height = tiles[0].length
   }
 }
+
+/*
+
+public Tile tile(int x, int y){
+  if (x < 0 || x >= width || y < 0 || y >= height)
+    return Tile.BOUNDS;
+  else
+    return tiles[x][y];
+}
+
+public char glyph(int x, int y){
+  return tile(x, y).glyph();
+}
+
+public Color color(int x, int y){
+  return tile(x, y).color();
+}
+
+*/
