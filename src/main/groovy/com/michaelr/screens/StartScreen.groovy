5ef2bc4f64f3067710734f1e06860a498653c6d8
Michael Robinson <seraph890@gmail.com>
2017-04-06 23:00:32 +1000

    Added necessary overrides

--- src/main/groovy/com/michaelr/screens/StartScreen.groovy
+++ src/main/groovy/com/michaelr/screens/StartScreen.groovy
@@ -4,12 +4,14 @@ import java.awt.event.KeyEvent
 import asciiPanel.AsciiPanel
 
 class StartScreen implements Screen {
-    void displayOutput(AsciiPanel terminal) {
-        terminal.write("rgpG -- Groovy Roguelike", 1, 1)
-        terminal.writeCenter("-- press [enter] to start --", 22)
-    }
+  @Override
+  void displayOutput(AsciiPanel terminal) {
+      terminal.write("rgpG -- Groovy Roguelike", 1, 1)
+      terminal.writeCenter("-- press [enter] to start --", 22)
+  }
 
-    Screen respondToUserInput(KeyEvent key) {
-        key.getKeyCode() == KeyEvent.VK_ENTER ? new GameScreen() : this
-    }
+  @Override
+  Screen respondToUserInput(KeyEvent key) {
+      key.getKeyCode() == KeyEvent.VK_ENTER ? new GameScreen() : this
+  }
 }
