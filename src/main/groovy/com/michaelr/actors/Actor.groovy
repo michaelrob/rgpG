13ec9959efda7f4b9ce8f2031689e080fe9da013
Michael Robinson <seraph890@gmail.com>
2017-04-06 23:14:30 +1000

    Added actor class and package

---
+++ src/main/groovy/com/michaelr/actors/Actor.groovy
@@ -0,0 +1,27 @@
+package com.michaelr.actors
+
+import java.awt.Color
+
+class Actor {
+  private World world
+
+  int x
+  int y
+
+  char glyph
+  Color color
+
+  char glyph() {
+    glyph
+  }
+
+  Color color() {
+    color
+  }
+
+  Actor(World world, char glyph, Color color) {
+    this.world = world
+    this.glyph = glyph
+    this.color = color
+  }
+}
